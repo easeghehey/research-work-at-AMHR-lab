f97c8f4fc44e26915b87d22e8301c765271c9443
easeghehey <emanuelaseghey9@gmail.com>
2022-09-20 13:08:39 -0400

    some more work

--- APIconnection.py
+++ APIconnection.py
@@ -1,15 +1,27 @@
 import os
 from dotenv import load_dotenv
 import openai
+import json
 
 load_dotenv()
 
 openai.organization = os.getenv('ORGANIZATION_ID')
 openai.api_key = os.getenv('API_KEY')
 
-prompt = "Who won the last football world cup?"
+prompt = "Write Java code for a function that creates a Magic The Gathering card that destroys all creatures with flying."
 
-response = openai.Completion.create(engine="davinci",max_tokens=25, prompt=prompt, temperature=0, 
-top_p=1, frequency_penalty=0,presence_penalty=0)
+response = openai.Completion.create(engine="davinci",max_tokens=200, prompt=prompt, temperature=0.5, 
+top_p=1.0, frequency_penalty=0.52,presence_penalty=0.5)
 
-print(response) # get the output
+# Write to Json
+
+jsonObject = json.dumps(response, indent = 4)
+
+with open("output.json", "w") as outfile:
+    outfile.write(jsonObject) 
+
+
+file = open('text.txt', 'w')
+file.write(response['choices'][0]['text'])
+
+# print(response) # get the output
