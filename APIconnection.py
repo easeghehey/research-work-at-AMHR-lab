a7648aaed68ed30b619aa44705d097d2ddb12c4c
easeghehey <emanuelaseghey9@gmail.com>
2022-10-04 22:44:26 -0400

    set cover

--- APIconnection.py
+++ APIconnection.py
@@ -8,7 +8,7 @@ load_dotenv()
 openai.organization = os.getenv('ORGANIZATION_ID')
 openai.api_key = os.getenv('API_KEY')
 
-prompt = "Write Java code for a function that creates a Magic The Gathering card that destroys all creatures with flying."
+prompt = "Hi my name is Emanuel, what's yours?"
 
 response = openai.Completion.create(engine="davinci",max_tokens=200, prompt=prompt, temperature=0.5, 
 top_p=1.0, frequency_penalty=0.52,presence_penalty=0.5)
