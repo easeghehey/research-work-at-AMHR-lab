ddce37bed987032321a81458b3b5401166655df4
easeghehey <emanuelaseghey9@gmail.com>
2022-10-31 12:08:10 -0400

    work

--- APIconnection.py
+++ APIconnection.py
@@ -8,7 +8,7 @@ load_dotenv()
 openai.organization = os.getenv('ORGANIZATION_ID')
 openai.api_key = os.getenv('API_KEY')
 
-prompt = "Hi my name is Emanuel, what's yours?"
+prompt = "Hello Logan is dying"
 
 response = openai.Completion.create(engine="davinci",max_tokens=200, prompt=prompt, temperature=0.5, 
 top_p=1.0, frequency_penalty=0.52,presence_penalty=0.5)
